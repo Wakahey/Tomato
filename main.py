1545a6872f03b3933136a50d5d29fd631fdfdb01
Danya <noobiko262@gmail.com>
2024-03-18 17:53:27 +0300

    Видеопотоки

--- main.py
+++ main.py
@@ -1,51 +1,77 @@
 import tkinter as tk
 from tkinter import ttk
-from webcam import Box
-import cv2
-import imutils
+from webcam import WebCamera
 
 class ProductionGUI:
+    """
+    Основной класс для работы с интерфейсом.
+    """
     def __init__(self, root):
         self.root = root
-        self.root.title("Система технического зрения для выявления ")
+        self.root.title("Система технического зрения для выявления брака у продукции")
 
-        # Создание фрейма для кнопок
+        # Создание объекта класса для работы с камерой.
+        self.video = WebCamera(root, width=800, height=600)
+
+        # Создание фрейма для кнопок.
         self.button_frame = tk.Frame(self.root)
         self.button_frame.pack()
 
+        # Создание стилей для кнопок (цвет, шрифт и т.д).
+        self.style = ttk.Style()
+        self.style.configure(
+            'Start.TButton', foreground='black',
+            background='green', font=('Arial', 12, 'bold')
+        )
+        self.style.configure(
+            'Pause.TButton', foreground='white',
+            background='orange', font=('Arial', 12, 'bold')
+        )
+        self.style.configure(
+            'Stop.TButton', foreground='black',
+            background='red', font=('Arial', 12, 'bold')
+        )
+
         # Создание кнопок
-        self.start_button = ttk.Button(self.button_frame, text="Старт", command=self.start_video)
+        self.start_button = ttk.Button(
+            self.button_frame, text="Старт",
+            command=self.start_video, style="Start.TButton"
+        )
         self.start_button.pack(side='left', padx=10, pady=5)
 
-        self.pause_button = ttk.Button(self.button_frame, text="Пауза", command=self.pause_video)
+        self.pause_button = ttk.Button(
+            self.button_frame, text="Пауза",
+            command=self.pause_video, style="Pause.TButton"
+        )
         self.pause_button.pack(side='left', padx=10, pady=5)
 
-        self.stop_button = ttk.Button(self.button_frame, text="Стоп", command=self.stop_video)
+        self.stop_button = ttk.Button(
+            self.button_frame, text="Стоп",
+            command=self.stop_video, style="Stop.TButton"
+        )
         self.stop_button.pack(side='left', padx=10, pady=5)
 
-        # Создание видеоэлемента
-        self.video = Box(root, width=800, height=600)
-
-        # Переменная для хранения состояния видео
+        # Переменная для хранения состояния видео.
         self.is_playing = False
 
         # Задание размера окна
         self.root.geometry("1500x900")
 
     def start_video(self):
-        # Начать воспроизведение видео
+        # Начать воспроизведение видео.
         self.video.start()
         self.is_playing = True
 
     def pause_video(self):
-        # Приостановить воспроизведение видео
+        # Приостановить воспроизведение видео.
         self.video.set_paused()
         self.is_playing = False
 
     def stop_video(self):
-        # Остановить воспроизведение видео
+        # Остановить воспроизведение видео.
         self.video.stop()
         self.is_playing = False
+
 
 if __name__ == "__main__":
     root = tk.Tk()
--- webcam.py
+++ webcam.py
@@ -3,10 +3,17 @@ from tkinter import Label
 from PIL import Image, ImageTk
 
 
-class Box:
-    """Box"""
+class WebCamera:
+    """Класс для работы с камерой"""
 
     def __init__(self, window, width=450, height=450):
+        """
+        Инициализация переменных в объект класса для работы с камерой.
+
+        :param window: объект окна
+        :param width: ширина окна
+        :param height: длина окна
+        """
         self.window = window
         self.width = width
         self.height = height
@@ -16,20 +23,20 @@ class Box:
         self.flag_paused = False
 
     def get_box_info(self):
-        """Gets Box Information """
+        """Получение информации по окну"""
         print(f"Window: {self.window}")
         print(f"Width: {self.width}")
         print(f"Height: {self.height}")
 
     def show_frames(self):
-        """Show Frames"""
+        """Метод класса показывающий изображение с камеры"""
         if self.showing_frames and not self.flag_paused:
             ret, frame = self.cap.read()
-            if ret:  # Check if frame was successfully obtained
+            if ret:
                 cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 img = Image.fromarray(cv2image)
 
-                imgtk = ImageTk.PhotoImage(image=img)  # Convert image to PhotoImage
+                imgtk = ImageTk.PhotoImage(image=img)
 
                 if self.label:
                     self.label.imgtk = imgtk
@@ -38,11 +45,10 @@ class Box:
                     self.label = Label(self.window, width=self.width, height=self.height, image=imgtk)
                     self.label.pack()
 
-            # Repeat after an interval to capture continuously
             self.label.after(20, self.show_frames)
 
     def start(self):
-        """Старт видео потока"""
+        """Метод класса запускающий поток камеры"""
         self.flag_paused = False
         self.cap = cv2.VideoCapture(0)
         self.label = Label(self.window, width=self.width, height=self.height)
@@ -51,13 +57,13 @@ class Box:
         self.show_frames()
 
     def set_paused(self):
-        """Пауза видео потока"""
+        """Метод класса останавливающий поток камеры"""
         self.flag_paused = True
 
     def stop(self):
-        """Стоп видео потока"""
+        """Метод класса останавливающий закрывающий поток камеры"""
         self.showing_frames = False
         if self.cap:
-            self.cap.release()  # Release the camera
+            self.cap.release()
         if self.label:
             self.label.destroy()
